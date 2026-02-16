7591bb0d2585ea8be4ad171a98180ad858701d42
Umang Garg <umanggarg0210@gmail.com>
2026-02-16 13:34:16 +0530

    .

--- file1.js
+++ file1.js
@@ -1,11 +1,5 @@
-let a = 10
-let b = 20
-let c = a + b
-console.log(c)
-// console.log("Hello World")
+console.log("Hello, World!");
 
-/*
-console.log("line 1")
-console.log("line 2")
-console.log("line 3")
-*/
+let name = "Alice";
+console.log("Hello, " + name + "!");
+console.log(`Hello, ${name}!`);
