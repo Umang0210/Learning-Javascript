6186709b2841a2ba8e89ef74f022b43f54aa17a8
Umang Garg <umanggarg0210@gmail.com>
2026-02-16 13:34:34 +0530

    .

--- file3.js
+++ file3.js
@@ -1,23 +1,39 @@
 let a = 10
 let b = 4
-//arithmetic operators
+#artimetic operators
 console.log(a + b)
 console.log(a - b)
 console.log(a * b)
 console.log(a / b)
 console.log(a % b)
 console.log(a ** b)
-//comparison operators
-console.log(a > b)
-console.log(a < b)
-console.log(a >= b)
-console.log(a <= b)
-console.log(a != b)
+#comparison operators
+console.log.log(a>b)
+console.log(a<b)
+console.log(a>=b)
+console.log(a<=b)
+console.log(a==b)
+console.log(a!=b)
+
 let x = "10"
 let y = 10
-console.log(x == y)
-console.log(x === y)
-//logical operators
-console.log(a > b && a > 20)
-console.log(a > b || a > 20)
-console.log(!a > b)
+console.log(x == y) #true
+console.log(x === y) #false
+
+#logical operators
+
+
+##l
+console.log(a > 5 && a>20) #true
+console.log(a > 5 || a>20) #true
+
+
+for(let i=5; i<=30; i+=5){
+    console.log(i)
+}
+
+
+##writea function to accept two numbers and print Sum
+function sum(num1, num2){
+    console.log(num1 + num2)
+}
