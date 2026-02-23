fa5b7b3b90af2e0e3c575dcbc9d4f815a504b1e0
Umang Garg <umanggarg0210@gmail.com>
2026-02-23 10:26:03 +0530

    Switches cases in js

---
+++ file23.js
@@ -0,0 +1,14 @@
+let score = 50
+switch (score) {
+    case 50:
+        console.log("Grade A");
+        break;
+    case 70:
+        console.log("Grade B");
+        break;
+    case 80:
+        console.log("Grade C");
+        break;
+    default:
+        console.log("Grade D");
+}
