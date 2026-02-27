abc604ce2d5c2a43c22e088a64214d1a8db24fc6
Umang Garg <umanggarg0210@gmail.com>
2026-02-27 10:01:47 +0530

    try catch functions

---
+++ file55.js
@@ -0,0 +1,8 @@
+try{
+    console.log(num);
+} catch(err){
+    // console.log(err)
+    // console.log(err.name)
+    console.log("Error: " + err.message);
+    //console.log("something well wrong")
+}
