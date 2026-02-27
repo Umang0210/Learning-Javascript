93b5c641fc3cb58873d87f0f761077b98cb6cbc8
Umang Garg <umanggarg0210@gmail.com>
2026-02-27 10:02:11 +0530

    for loop

---
+++ file56.js
@@ -0,0 +1,13 @@
+const employees = ["john", "cathy", "mike"];
+for(let e in employees){
+    // console.log(e)
+    console.log(employees[e]);
+}
+
+const student = {
+    name : "john",
+    age : 23
+}
+for (let s in student){
+    consolfe.log(s);
+}
